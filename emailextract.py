6589d53c11c4e1ac72e0012a9ae5451d55bcfd3f
walula <25298273+walula@users.noreply.github.com>
2026-06-15 11:43:47 +0200

    Add Burp proxy support to email extraction script

--- emailextract.py
+++ emailextract.py
@@ -1,20 +1,18 @@
 # Extraction of the Global Address List (GAL) on Exchange >=2013 servers via Outlook Web Access (OWA) 
-# By Pigeonburger, June 2021
-# https://github.com/pigeonburger
+# By Pigeonburger, June 2021 | Modified for Burp proxy support
 
-# module import heehoo
 import requests, json, argparse
+import urllib3
 
-# argparser hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-parser = argparse.ArgumentParser(description="Extract the Global Address List (GAL) on Exchange 2013 servers via Outlook Web Access (OWA)")
-parser.add_argument("-i", "--host", dest="hostname",
-                  help="Hostname for the Exchange Server", metavar="HOSTNAME", type=str, required=True)
-parser.add_argument("-u", "--username", dest="username",
-                  help="A username to log in", metavar="USERNAME", type=str, required=True)
-parser.add_argument("-p", "--password", dest="password",
-                  help="A password to log in", metavar="PASSWORD", type=str, required=True)
-parser.add_argument("-o", "--output-file", dest="output",
-                  help="Specify file to output emails to (default is global_address_list.txt)", metavar="OUTPUT FILE", type=str, default="global_address_list.txt")
+# Suppress SSL warnings when using Burp's self-signed cert
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+parser = argparse.ArgumentParser(description="Extract the GAL on Exchange 2013+ via OWA")
+parser.add_argument("-i", "--host", dest="hostname", help="Hostname for the Exchange Server", metavar="HOSTNAME", type=str, required=True)
+parser.add_argument("-u", "--username", dest="username", help="A username to log in", metavar="USERNAME", type=str, required=True)
+parser.add_argument("-p", "--password", dest="password", help="A password to log in", metavar="PASSWORD", type=str, required=True)
+parser.add_argument("-o", "--output-file", dest="output", help="Output file (default: global_address_list.txt)", metavar="OUTPUT FILE", type=str, default="global_address_list.txt")
+parser.add_argument("--proxy", dest="proxy", help="Proxy URL for interception (e.g. http://127.0.0.1:8080)", metavar="PROXY", type=str, default=None)
 
 args = parser.parse_args()
 
@@ -22,15 +20,20 @@ url = args.hostname
 USERNAME = args.username
 PASSWORD = args.password
 OUTPUT = args.output
+PROXY = args.proxy
 
+# Build proxy dict for requests
+proxies = {"http": PROXY, "https": PROXY} if PROXY else {}
 
 # Start the session
 s = requests.Session()
+s.proxies.update(proxies)
+s.verify = False  # Disable SSL verification for Burp's self-signed cert
+
 print("Connecting to %s/owa" % url)
+if PROXY:
+    print("Routing traffic through proxy: %s" % PROXY)
 
-
-# Get OWA landing page
-# Add https:// scheme if not already added in the --host arg
 try:
     s.get(url+"/owa")
     URL = url
@@ -38,48 +41,30 @@ except requests.exceptions.MissingSchema:
     s.get("https://"+url+"/owa")
     URL = "https://"+url
 
-
-# Other URLs we need later
 AUTH_URL = URL+"/owa/auth.owa"
 PEOPLE_FILTERS_URL = URL + "/owa/service.svc?action=GetPeopleFilters"
 FIND_PEOPLE_URL = URL + "/owa/service.svc?action=FindPeople"
 
-
-# Attempt a login to OWA
 login_data={"username":USERNAME, "password":PASSWORD, 'destination': URL, 'flags': '4', 'forcedownlevel': '0'}
 r = s.post(AUTH_URL, data=login_data, headers={'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"})
 
-
-# The Canary is a unique ID thing provided upon a successful login that's also required in the header for the next few requests to be successful.
-# Even upon an incorrect login, OWA still gives a 200 status, so we can also check if the login was successful by seeing if this cookie was set or not.
 try:
     session_canary = s.cookies['X-OWA-CANARY']
 except:
     exit("\nInvalid Login Details. Login Failed.")
 print("\nLogin Successful!\nCanary key:", session_canary)
 
-
-# Returns an object containing the IDs of all accessible address lists, so we can specify one in the FindPeople request
 r = s.post(PEOPLE_FILTERS_URL, headers={'Content-type': 'application/json', 'X-OWA-CANARY': session_canary, 'Action': 'GetPeopleFilters'}, data={}).json()
 
-
-# Find the Global Address List id
 for i in r:
     if i['DisplayName'] == "Default Global Address List":
         AddressListId = i['FolderId']['Id']
         print("Global List Address ID:", AddressListId)
         break
 
-
-# Set to None to return all emails in the list (this is the search term for the FindPeople request)
 query = None
-
-
-# Set the max results for the FindPeople request.
 max_results = 99999
 
-
-# POST data for the FindPeople request
 peopledata = {
     "__type": "FindPeopleJsonRequest:#Exchange",
     "Header": {
@@ -117,12 +102,8 @@ peopledata = {
     }
 }
 
-
-# Make da request.
 r = s.post(FIND_PEOPLE_URL, headers={'Content-type': 'application/json', 'X-OWA-CANARY': session_canary, 'Action': 'FindPeople'}, data=json.dumps(peopledata)).json()
 
-
-# Parse out the emails, print them and append them to a file.
 userlist = r['Body']['ResultSet']
 
 with open(OUTPUT, 'a+') as outputfile:
